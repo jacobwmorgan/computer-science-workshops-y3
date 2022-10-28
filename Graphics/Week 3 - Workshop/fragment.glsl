e640aae2f6086951787e27fc586efe5bf312e12e
Alex <alex@gu3.me>
2022-10-28 19:27:25 +0100

    Add some commenting to Graphics Week 3, needs some extra stuff adding from the workshop

--- Graphics/Week 3 - Workshop/fragment.glsl
+++ Graphics/Week 3 - Workshop/fragment.glsl
@@ -1,32 +1,48 @@
 precision highp float;
 precision highp int;
 
+// Get the UV co-ordinates from the vertex shader
 varying vec2 uvs;
 
+// We want to be able to scale things over time
 uniform float time;
+
+// We want to be able to use our own texture
 uniform sampler2D myTexture;
 
+// This is a basic luminance function.
+// It returns the grayscale value of a particular color.
+// It is weighted more towards green because of how the human eye perceives color.
 float GetGrayValue( vec3 color )
 {
-    float grayValue = 0.21 * color.r;
-    grayValue += 0.72 * color.g;
-    grayValue += 0.07 * color.b;
-     
+    // These color weights are approximations at 2dp, but this is good enough for our purposes.
+
+    float grayValue = 0.21 * color.r;   // 21% red
+    grayValue += 0.72 * color.g;        // 72% green
+    grayValue += 0.07 * color.b;        // 7% blue
     return grayValue;
 }
 
-void main() {
-    
+void main() 
+{   
+    // We want to modify the UVs.. so we make a copy, because we
+    // can't modify variables that are "varying" (see above). 
     vec2 uv = uvs;
+
+    // Wobble UVs over time
     uv += vec2( sin( time ), sin( time * 2.0 ) ) * 0.1;
     
+    // Get the color from the texture
     vec3 col = texture2D( myTexture, uv ).xyz;
     
+    // We want to create a vignette effect.. here are the steps we need to do:
+    // Step 1: create a circle, centered at (0.5, 0.5)
     float v = length( uvs - 0.5 );
+    // Step 2: Reduce the size of the circle so that it's twice as big
     v *= 2.0;
+    // Step 3: use this circle as a 'mask', subtracting away from the color
     col *= 1.0 - v;
     
-
-    gl_FragColor = vec4( vec3( col ), 1.0 );
-
+    // End result goes in gl_FragColor.
+    gl_FragColor = vec4( col, 1.0 );
 }
--- Graphics/Week 3 - Workshop/vertex.glsl
+++ Graphics/Week 3 - Workshop/vertex.glsl
@@ -1,18 +1,26 @@
 precision highp float;
 precision highp int;
 
-varying vec2 uvs;
-
 uniform mat4 modelViewMatrix;
 uniform mat4 projectionMatrix;
 
+// Pass the UV co-ordinates to the fragment shader
+varying vec2 uvs;
+
+// ShaderFrog should give us vertex positions
 attribute vec3 position;
+
+// ShaderFrog should give us UV co-ordinates
 attribute vec2 uv;
 
-void main() {
-    
+void main() 
+{    
+    // Pass the UV co-ordinates to the fragment shader
     uvs = uv;
 
+    // Multiplying 3D co-ordinates by a projection & model-view matrix will
+    // 'project' those 3D co-ordinates into 2D space for viewing on a 
+    // 2D monitor.
+    // You don't need to worry about any of this right now.
     gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-
 }
